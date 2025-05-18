3a0c29ee72caf631e9fe17ae95348942daac3d82
kaznotaninokaz <kazzzzz0358@gmail.com>
2025-05-18 13:05:22 +0900

    フィードバックにより修正

--- assets/script/main.js
+++ assets/script/main.js
@@ -4,7 +4,7 @@ $('.voices__slick').slick({
   arrows: true,
   slidesToShow: 3,
   slidesToScroll: 3,
-  prevArrow: '<img src="/assets/image/voice/arrow_left_circle_fill.png"  class="slick-prev bi bi-arrow-left"></い>',
+  prevArrow: '<img src="/assets/image/voice/arrow_left_circle_fill.png"  class="slick-prev bi bi-arrow-left"></img>',
   nextArrow: '<img src="/assets/image/voice/arrow_right_circle_fill.png"  class="slick-next bi bi bi-arrow-right"></img>',
   centerMode: false,
   infinite: false,
@@ -20,12 +20,3 @@ $('.voices__slick').slick({
 });
 
 
-// アコーディオンのタイトルがクリックされたら
-$(function () {
-  $('.question__content-accordion-top').on('click', function () {
-    var content = $(this).next();
-    content.slideToggle(); // アコーディオンの開閉
-    $(this).toggleClass('open'); // クラス切替（必要ならスタイル制御）
-    $(this).find('i').toggleClass('rotate'); // 矢印を回転
-  });
-});
